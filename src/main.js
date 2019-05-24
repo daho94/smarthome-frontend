b58f1b459f826e0761337e452163f1503b08d2ee
Daniel Holzner <danielholzner@gmx.de>
2019-05-24 15:09:25 +0200

    Added bootstrap

--- src/main.js
+++ src/main.js
@@ -4,9 +4,13 @@ import PortalVue from 'portal-vue'
 import store from './vuex/store'
 import VueSocketIO from 'vue-socket.io'
 import { router } from './router'
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 Vue.config.productionTip = false
 
+Vue.use(BootstrapVue)
 Vue.use(PortalVue)
 Vue.use(new VueSocketIO({
   debug: true,
