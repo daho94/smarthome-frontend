7276caf970b57806cbd441f9d9c407c78dcde77c
Daniel Holzner <danielholzner@gmx.de>
2019-11-10 00:01:42 +0100

    Add tv app launcher widget

---
+++ src/components/WidgetAppLauncher.vue
@@ -0,0 +1,35 @@
+<template>
+  <section>
+    <app-icon style="width:100%" icon="youtube" class="centered"/>
+  </section>
+</template>
+
+<script>
+import AppIcon from './AppIcon'
+
+export default {
+  name: 'widget-app-launcher',
+  components: {
+    AppIcon
+  },
+  data() {
+    return {
+      lgtv: undefined
+    }
+  },
+  mounted() {
+  
+  }
+}
+</script>
+
+<style lang="scss">
+.centered {
+  // transform: translate(-15%, 0px);
+  display:flex;
+  justify-content: center;
+  >use {
+    justify-content: center;
+  }
+}
+</style>
